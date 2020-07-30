716435a902c3eeec2c0af9be21d80dd13d043cbc
Felipe Fadul <felipe.fadul@hotmail.com>
2020-07-29 21:27:55 -0300

    Adiciona aula da seção 20

---
+++ 20-hapi-jwt/src/routes/authRoutes.js
@@ -0,0 +1,61 @@
+// npm i jsonwebtoken
+
+const BaseRoute = require('./base/baseRoute');
+const Joi = require('joi');
+const Boom = require('boom');
+const Jwt = require('jsonwebtoken');
+
+const failAction = (request, headers, error) => {
+  throw error;
+}
+
+const USER = {
+  username: 'teste123',
+  password: '123'
+};
+
+class AuthRoutes extends BaseRoute {
+  constructor(secret) {
+    super();
+    this.secret = secret;
+  }
+
+  login() {
+    return {
+      path: '/login',
+      method: 'POST',
+      config: {
+        auth: false,
+        tags: ['api'],
+        description: 'Obter token.',
+        notes: 'Realiza login com user e senha do banco.',
+        validate: {
+          payload: {
+            username: Joi.string().required(),
+            password: Joi.string().required()
+          },
+          failAction
+        }
+      },
+      handler: async (request) => {
+        const { username, password } = request.payload;
+
+        if(username.toLowerCase() !== USER.username.toLowerCase() ||
+           password !== USER.password)
+          return Boom.unauthorized();
+
+        const token = Jwt.sign({
+          username: username,
+          id: 1
+        }, this.secret);
+
+        return {
+          token
+        };
+      }
+    };
+  }
+
+}
+
+module.exports = AuthRoutes;
